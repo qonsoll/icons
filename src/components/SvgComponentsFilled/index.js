54e62b4fe8f77777dfad39f7d8aefdcb2de4d50b
sashka2131 <uhucbeucg@gmail.com>
2022-01-13 18:19:07 +0200

    add svg icons and react components

---
+++ src/components/SvgComponentsFilled/index.js
@@ -0,0 +1,264 @@
+import ActivitiesFilled from './Activities'
+import AlertDangerWarningFilled from './AlertDangerWarning'
+import AlertInfoFilled from './AlertInfo'
+import AppointmentFilled from './Appointment'
+import ArrowLongDownFilled from './ArrowLongDown'
+import ArrowLongLeftFilled from './ArrowLongLeft'
+import ArrowLongRightFilled from './ArrowLongRight'
+import ArrowLongUpFilled from './ArrowLongUp'
+import ArrowShortDownFilled from './ArrowShortDown'
+import ArrowShortLeftFilled from './ArrowShortLeft'
+import ArrowShortRightFilled from './ArrowShortRight'
+import ArrowShortUpFilled from './ArrowShortUp'
+import BioflowFilled from './Bioflow'
+import BioGroupFilled from './BioGroup'
+import BroomFilled from './Broom'
+import CalendarFilled from './Calendar'
+import CameraFilled from './Camera'
+import CategoryFilled from './Category'
+import ChartBarFilled from './ChartBar'
+import ChartPieFilled from './ChartPie'
+import CheckmarkFilled from './Checkmark'
+import ClinicFilled from './Clinic'
+import ClockFilled from './Clock'
+import CloseFilled from './Close'
+import DisorderFilled from './Disorder'
+import DocFilled from './Doc'
+import EditFilled from './Edit'
+import EvaluationStateFilled from './EvaluationState'
+import FileFilled from './File'
+import FileAddFilled from './FileAdd'
+import FileDownloadFilled from './FileDownload'
+import FileFailFilled from './FileFail'
+import FileUploadFilled from './FileUpload'
+import FilterFilled from './Filter'
+import FoihelseFilled from './Foihelse'
+import FolderFilled from './Folder'
+import FormFilled from './Form'
+import HeartFilled from './Heart'
+import HideFilled from './Hide'
+import HistoryFilled from './History'
+import HomeFilled from './Home'
+import ImageFilled from './Image'
+import InvoiceFilled from './Invoice'
+import LocationFilled from './Location'
+import LockFilled from './Lock'
+import LoginFilled from './Login'
+import LogoutFilled from './Logout'
+import MailFilled from './Mail'
+import MenuFilled from './Menu'
+import MessageFilled from './Message'
+import MessageSendFilled from './MessageSend'
+import MicrophoneOffFilled from './MicrophoneOff'
+import MicrophoneOnFilled from './MicrophoneOn'
+import MinusFilled from './Minus'
+import NotificationFilled from './Notification'
+import PackageFilled from './Package'
+import PasswordFilled from './Password'
+import PatientFilled from './Patient'
+import PatientAddFilled from './PatientAdd'
+import PatientsFilled from './Patients'
+import PhoneFilled from './Phone'
+import PhoneCallingFilled from './PhoneCalling'
+import PhoneCallMissedFilled from './PhoneCallMissed'
+import PhoneCallMuttedFilled from './PhoneCallMutted'
+import PlayFilled from './Play'
+import PlusFilled from './Plus'
+import SearchFilled from './Search'
+import SendFilled from './Send'
+import SettingsFilled from './Settings'
+import ShowFilled from './Show'
+import StarFilled from './Star'
+import StudyFilled from './Study'
+import TherapistFilled from './Therapist'
+import TherapistAddFilled from './TherapistAdd'
+import Ticket1Filled from './Ticket1'
+import Ticket2Filled from './Ticket2'
+import TranslationFilled from './Translation'
+import TrashFilled from './Trash'
+import UnlockFilled from './Unlock'
+import VideoOffFilled from './VideoOff'
+import VideoOnFilled from './VideoOn'
+import VippsFilled from './Vipps'
+import VolumeDownFilled from './VolumeDown'
+import VolumeOffFilled from './VolumeOff'
+import VolumeUpFilled from './VolumeUp'
+import WalletFilled from './Wallet'
+
+export {
+  ActivitiesFilled,
+  AlertDangerWarningFilled,
+  AlertInfoFilled,
+  AppointmentFilled,
+  ArrowLongDownFilled,
+  ArrowLongLeftFilled,
+  ArrowLongRightFilled,
+  ArrowLongUpFilled,
+  ArrowShortDownFilled,
+  ArrowShortLeftFilled,
+  ArrowShortRightFilled,
+  ArrowShortUpFilled,
+  BioflowFilled,
+  BioGroupFilled,
+  BroomFilled,
+  CalendarFilled,
+  CameraFilled,
+  CategoryFilled,
+  ChartBarFilled,
+  ChartPieFilled,
+  CheckmarkFilled,
+  ClinicFilled,
+  ClockFilled,
+  CloseFilled,
+  DisorderFilled,
+  DocFilled,
+  EditFilled,
+  EvaluationStateFilled,
+  FileFilled,
+  FileAddFilled,
+  FileDownloadFilled,
+  FileFailFilled,
+  FileUploadFilled,
+  FilterFilled,
+  FoihelseFilled,
+  FolderFilled,
+  FormFilled,
+  HeartFilled,
+  HideFilled,
+  HistoryFilled,
+  HomeFilled,
+  ImageFilled,
+  InvoiceFilled,
+  LocationFilled,
+  LockFilled,
+  LoginFilled,
+  LogoutFilled,
+  MailFilled,
+  MenuFilled,
+  MessageFilled,
+  MessageSendFilled,
+  MicrophoneOffFilled,
+  MicrophoneOnFilled,
+  MinusFilled,
+  NotificationFilled,
+  PackageFilled,
+  PasswordFilled,
+  PatientFilled,
+  PatientAddFilled,
+  PatientsFilled,
+  PhoneFilled,
+  PhoneCallingFilled,
+  PhoneCallMissedFilled,
+  PhoneCallMuttedFilled,
+  PlayFilled,
+  PlusFilled,
+  SearchFilled,
+  SendFilled,
+  SettingsFilled,
+  ShowFilled,
+  StarFilled,
+  StudyFilled,
+  TherapistFilled,
+  TherapistAddFilled,
+  Ticket1Filled,
+  Ticket2Filled,
+  TranslationFilled,
+  TrashFilled,
+  UnlockFilled,
+  VideoOffFilled,
+  VideoOnFilled,
+  VippsFilled,
+  VolumeDownFilled,
+  VolumeOffFilled,
+  VolumeUpFilled,
+  WalletFilled
+}
+
+export default {
+  ActivitiesFilled,
+  AlertDangerWarningFilled,
+  AlertInfoFilled,
+  AppointmentFilled,
+  ArrowLongDownFilled,
+  ArrowLongLeftFilled,
+  ArrowLongRightFilled,
+  ArrowLongUpFilled,
+  ArrowShortDownFilled,
+  ArrowShortLeftFilled,
+  ArrowShortRightFilled,
+  ArrowShortUpFilled,
+  BioflowFilled,
+  BioGroupFilled,
+  BroomFilled,
+  CalendarFilled,
+  CameraFilled,
+  CategoryFilled,
+  ChartBarFilled,
+  ChartPieFilled,
+  CheckmarkFilled,
+  ClinicFilled,
+  ClockFilled,
+  CloseFilled,
+  DisorderFilled,
+  DocFilled,
+  EditFilled,
+  EvaluationStateFilled,
+  FileFilled,
+  FileAddFilled,
+  FileDownloadFilled,
+  FileFailFilled,
+  FileUploadFilled,
+  FilterFilled,
+  FoihelseFilled,
+  FolderFilled,
+  FormFilled,
+  HeartFilled,
+  HideFilled,
+  HistoryFilled,
+  HomeFilled,
+  ImageFilled,
+  InvoiceFilled,
+  LocationFilled,
+  LockFilled,
+  LoginFilled,
+  LogoutFilled,
+  MailFilled,
+  MenuFilled,
+  MessageFilled,
+  MessageSendFilled,
+  MicrophoneOffFilled,
+  MicrophoneOnFilled,
+  MinusFilled,
+  NotificationFilled,
+  PackageFilled,
+  PasswordFilled,
+  PatientFilled,
+  PatientAddFilled,
+  PatientsFilled,
+  PhoneFilled,
+  PhoneCallingFilled,
+  PhoneCallMissedFilled,
+  PhoneCallMuttedFilled,
+  PlayFilled,
+  PlusFilled,
+  SearchFilled,
+  SendFilled,
+  SettingsFilled,
+  ShowFilled,
+  StarFilled,
+  StudyFilled,
+  TherapistFilled,
+  TherapistAddFilled,
+  Ticket1Filled,
+  Ticket2Filled,
+  TranslationFilled,
+  TrashFilled,
+  UnlockFilled,
+  VideoOffFilled,
+  VideoOnFilled,
+  VippsFilled,
+  VolumeDownFilled,
+  VolumeOffFilled,
+  VolumeUpFilled,
+  WalletFilled
+}
---
+++ src/components/SvgComponentsOutline/index.js
@@ -0,0 +1,4 @@
+import PlusOutlined from './Plus'
+
+export { PlusOutlined }
+export default { PlusOutlined }
