83e4f74466246190f1acdc0a0b8f027c0486235c
sashka2131 <uhucbeucg@gmail.com>
2022-01-13 18:06:20 +0200

    added config files

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import commonjs from 'rollup-plugin-commonjs'
+import babel from 'rollup-plugin-babel'
+import { terser } from 'rollup-plugin-terser'
+import pkg from './package.json'
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs'
+    },
+    {
+      file: pkg.module,
+      format: 'es'
+    }
+  ],
+  plugins: [
+    // Preferably set as first plugin.
+    peerDepsExternal(),
+    babel({
+      plugins: ['@babel/plugin-syntax-jsx'],
+      exclude: 'node_modules/**',
+      presets: ['@babel/env', '@babel/preset-react']
+    }),
+    commonjs(),
+    terser()
+  ]
+}
