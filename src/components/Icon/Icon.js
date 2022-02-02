f773f787b8bdc052b5a0b02a23c8d8611ce7a81c
sashka2131 <uhucbeucg@gmail.com>
2022-02-02 12:14:23 +0200

    add Box as icon wrapper, pass all possible props for box

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,16 +1,17 @@
-import React, { Fragment, cloneElement } from 'react'
+import React, { cloneElement } from 'react'
 import ICONS_NAMES from '../../constants/namesMap'
 import PROP_TYPES from '../../constants/iconPropTypes'
+import { Box } from '@qonsoll/react-design'
 
 const Icon = (props) => {
-  const { name, fill, size } = props
+  const { name, fill, size, minWidth, minHeight, ...rest } = props
 
   const isIconExist = !!ICONS_NAMES?.[name]
 
   return (
-    <Fragment>
+    <Box minWidth={minWidth || size} minHeight={minHeight || size} {...rest}>
       {isIconExist ? cloneElement(ICONS_NAMES?.[name], { fill, size }) : null}
-    </Fragment>
+    </Box>
   )
 }
 
