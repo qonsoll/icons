16aa8236b79dcd5f1535c908a7f264758a13bcb3
Olexandr25 <oleksandrvoichyshyn@gmail.com>
2022-09-26 20:28:44 +0300

    Added new Icons

--- src/components/SvgComponentsOutline/ArrowNarrowDown.js
+++ src/components/SvgComponentsOutline/ArrowNarrowDown.js
@@ -1,15 +1,25 @@
- import React from 'react'
-  
-  const ArrowNarrowDownOutlined = (props) => {
-    const { size = '24' , fill = 'black' } = props
-    
-    return (
-      <svg width={size} height={size} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M12 4V20M12 20L18 14M12 20L6 14" stroke={fill} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
+import React from 'react'
 
-    )
-  }
+const ArrowNarrowDownOutlined = (props) => {
+  const { size = '24', fill = 'black' } = props
 
-  export default ArrowNarrowDownOutlined 
-  
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M12 4V20M12 20L18 14M12 20L6 14"
+        stroke={fill}
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  )
+}
+
+export default ArrowNarrowDownOutlined
